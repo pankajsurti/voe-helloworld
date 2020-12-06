34a1c2a2a337a921932db3e08cfc95d8130946fc
Pankaj Surti <pankajsu@microsoft.com>
2020-12-06 17:48:13 -0600

    This is a VOE change in description

--- src/webparts/voeHelloworld/VoeHelloworldWebPart.ts
+++ src/webparts/voeHelloworld/VoeHelloworldWebPart.ts
@@ -21,7 +21,7 @@ export default class VoeHelloworldWebPart extends BaseClientSideWebPart<IVoeHell
         <div class="${ styles.container }">
           <div class="${ styles.row }">
             <div class="${ styles.column }">
-              <span class="${ styles.title }">Welcome to SharePoint!</span>
+              <span class="${ styles.title }">Welcome to SharePoint! VOE Demo 12/06/2020</span>
               <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
               <p class="${ styles.description }">${escape(this.properties.description)}</p>
               <a href="https://aka.ms/spfx" class="${ styles.button }">
